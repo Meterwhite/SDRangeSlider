0e57334d39bf8f1de6469818ad092b32c5b68471
NOVO <quxingyi@outlook.com>
2018-01-05 01:43:27 +0800

    first commit
    first commit

--- SDRangeSliderView/ViewController.m
+++ SDRangeSliderView/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "SDRangeSliderView.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    SDRangeSliderView* slider = [[SDRangeSliderView alloc] initWithFrame:CGRectMake(0, 100, 300, 50)];
+    
+    /*
+     *打开每个注释观察变化(Open comment to observe the change)
+     */
+//    slider.minimumSize = 10;
+    
+//    slider.offsetOfAdjustLineStart = -12;//look at viewHierarcy
+    
+//    slider.offsetOfAdjustLineEnd = 15;//look at viewHierarcy
+    
+//    [slider usingValueAtCenter];//look at viewHierarcy
+    
+//    [slider customUIUsingBlock:^(UIButton *leftCursor, UIButton *rightCursor) {
+//        [leftCursor setImage:nil forState:UIControlStateNormal];
+//        leftCursor.backgroundColor = UIColor.redColor;
+//        [rightCursor setImage:nil forState:UIControlStateNormal];
+//        rightCursor.backgroundColor = UIColor.redColor;
+//    }];
+    
+    [self.view addSubview:slider];
+    
+    UILabel* info = [[UILabel alloc] initWithFrame:CGRectMake(15, 64, 300, 20)];
+    info.text = @"SDRangeSliderView";
+    [self.view addSubview:info];
+    
+    [slider eventValueDidChanged:^(double left, double right) {
+        info.text = [NSString stringWithFormat:@"%@~%@",@(left),@(right)];
+    }];
 }
 
 
