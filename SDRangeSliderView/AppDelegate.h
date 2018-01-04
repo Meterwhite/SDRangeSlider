99966ed806737caae99ee8b8e43284c0986985bd
NOVO <quxingyi@outlook.com>
2018-01-04 20:56:02 +0800

    Initial Commit

---
+++ SDRangeSliderView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDRangeSliderView
+//
+//  Created by NOVO on 2018/1/4.
+//  Copyright © 2018年 NOVO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDRangeSliderView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDRangeSliderView
+//
+//  Created by NOVO on 2018/1/4.
+//  Copyright © 2018年 NOVO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
